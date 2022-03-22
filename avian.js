95e387d3782335f656c4bc24f09ad3f963c92881
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-03-22 18:52:15 -0400

    Add ChromeRequest

--- avian.js
+++ avian.js
@@ -1,13 +1,41 @@
 var avian = {
   call: function(data) {
-      notifyContent(data.method, data.args)
+      return notifyContent(data.method, data.args)
   },
   getBal: function() {
-      notifyContent("avn_balance", {});
+      return notifyContent("avn_balance", {});
   }
 }
 
+var ChromeRequest = (function(){
+  var requestId = 0;
+
+  function getData(data) {
+    var id = requestId++;
+
+    return new Promise(function(resolve, reject) {
+      var listener = function(evt) {
+        console.log(evt);
+        if(evt.detail.requestId == id) {
+          // Deregister self
+          window.removeEventListener("sendChromeData", listener);
+          resolve(evt.detail.data);
+        }
+      }
+
+      window.addEventListener("sendChromeData", listener);
+
+      var payload = { data: data, id: id };
+
+      window.dispatchEvent(new CustomEvent("getChromeData", {detail: payload}));
+    });        
+  }
+
+  return { getData: getData };
+})();
+
 function notifyContent(method, args) {
-  var evt = new CustomEvent("sendAvianData", {detail: {method, args}});
-  window.dispatchEvent(evt);        
+  ChromeRequest.getData({method, args}).then(function(data){
+    alert(data);
+  });
 }
--- contentscript.js
+++ contentscript.js
@@ -1,18 +1,22 @@
 // Inject Avian
 var s = document.createElement('script');
-s.src = browser.runtime.getURL('avian.js');
+s.src = chrome.runtime.getURL('avian.js');
 s.onload = function () {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
 
-window.addEventListener("sendAvianData", function (e) {
-    var data = e.detail;
-    var send = notifyBackground(data.method, data.args);
-    send.then(
-        message => alert(message.response),
-        error => console.log(`Error: ${error}`)
-    );
+window.addEventListener("getChromeData", function(evt) {
+  var request = evt.detail.data;
+  var send = notifyBackground(request.method, request.args);
+  send.then(
+      message => {
+        alert("Content Script: " + message.response) 
+        var response = {requestId: request.id, data: message};
+        window.dispatchEvent(new CustomEvent("sendChromeData", {detail: response}));
+      },
+      error => console.log(`Error: ${error}`)
+  );
 }, false);
 
 function notifyBackground(method, args) {
@@ -22,4 +26,3 @@ function notifyBackground(method, args) {
     });
     return send;
 }
-console.log(message.response);
