bde44ba3191f0af40b0dd00845f4481dea929f8c
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-04-05 23:08:07 -0400

    Add WindowPostMessageStream

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,11 @@
-// Inject Avian
+// Inject JS
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('stream.js');
+s.onload = function () {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
+
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('avian.js');
 s.onload = function () {
@@ -6,23 +13,21 @@ s.onload = function () {
 };
 (document.head || document.documentElement).appendChild(s);
 
-window.addEventListener("getChromeData", function(evt) {
-  var request = evt.detail.data;
-  var send = notifyBackground(request.method, request.args);
-  send.then(
-      message => {
-        alert("Content Script: " + message.response) 
-        var response = {requestId: request.id, data: message};
-        window.dispatchEvent(new CustomEvent("sendChromeData", {detail: response}));
-      },
-      error => console.log(`Error: ${error}`)
-  );
-}, false);
+const avianStream = new PostMessageStream.WindowPostMessageStream({
+  name: 'background',
+  target: 'avian',
+});
+    
+avianStream.on('data', (data) => {
+    let method = data.method;
+    let args = data.args;
 
-function notifyBackground(method, args) {
-    var send = chrome.runtime.sendMessage({
-        method: method,
-        args: args
-    });
-    return send;
-}
+    switch (method) {
+        case "avn_balance":
+            avianStream.write({result: "200"})
+            break;    
+        default:
+            avianStream.write({result: "N/A"})
+            break;
+    }
+});
